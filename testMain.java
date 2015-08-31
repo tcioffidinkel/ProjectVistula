a9966acb3d50aab74f5cd1d98eb2937b138c4c26
tcioffidinkel <tcioffidinkel@gmail.com>
2015-08-31 13:30:18 -0700

    Update testMain.java

--- testMain.java
+++ testMain.java
@@ -1,10 +1,7 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-
-import org.omg.CORBA.portable.InputStream;
 
 
 public class testMain {
@@ -13,7 +10,9 @@ public class testMain {
 	public static void main(String[] args) throws IOException
 	{
 		System.out.println("This");
-		
+		/*This can be used with all online documents you just have to replace the "URL" 
+		with the URL you want and then change the "File" to a file you want 
+		to save with matching suffix */
 		URL url = new URL("http://www.sec.gov/Archives/edgar/data/1288776/000128877615000039/a20150810form8-k.htm");
 		java.io.InputStream in = url.openStream();
 		FileOutputStream fos = new FileOutputStream(new File("yourFile2.htm"));
